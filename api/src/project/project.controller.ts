5271406da85e19dd9baecfb9d3205f278273d0d6
NareshNandigama <nandigama.naresh@gmail.com>
2026-09-12 23:58:47 +0530

    feat: add role based authorization

--- api/src/project/project.controller.ts
+++ api/src/project/project.controller.ts
@@ -4,14 +4,23 @@ import { ProjectService } from './project.service.js';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth/jwt-auth.guard.js';
 import { CurrentUser } from '../auth/decorators/current-user.decorator.js';
 import type { JwtPayload } from '../auth/types/jwt-payload.js';
+import { Roles } from '../auth/decorators/roles.decorator.js';
+import { RolesGuard } from '../auth/guards/roles/roles.guard.js';
 @Controller()
 export class ProjectController {
     constructor(private readonly projectServiece: ProjectService) {}
+    
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles('ADMIN')
     @Post("organizations/:organizationId/projects")
     create(
-            @Param('organizationId', new ParseUUIDPipe()) organizationId: string,
-            @Body() createProjectDto: CreateProjectDto) {
-        return this.projectServiece.create(organizationId, createProjectDto);
+    @Param('organizationId', new ParseUUIDPipe()) organizationId: string,
+    @Body() createProjectDto: CreateProjectDto,
+    ) {
+    return this.projectServiece.create(
+        organizationId,
+        createProjectDto,
+    );
     }
 
     @Get("organizations/:organizationId/projects")
